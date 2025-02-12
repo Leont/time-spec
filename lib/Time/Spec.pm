3859ffbde6a89b92bbab10d6912ec01881f8fe40
Leon Timmermans <fawaka@gmail.com>
2025-02-12 15:47:17 +0100

    Split Time::Spec out of Signal-Info

--- lib/Time/Spec.pm
+++ lib/Time/Spec.pm
@@ -3,8 +3,50 @@ package Time::Spec;
 use strict;
 use warnings;
 
+use overload
+	'0+'     => \&to_float,
+	fallback => 1;
+
 use XSLoader;
 
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
 1;
+
+# ABSTRACT: a wrapper arount struct timespec
+
+=head1 SYNOPSIS
+
+ my $spec = Time::Spec->new(1.5);
+ my_sleeper($spec);
+ say $spec->to_float;
+
+=head1 DESCRIPTION
+
+This holds a time specification, broken down into seconds and nanoseconds. This is typically used by XS modules and not by pure-perl ones.
+
+The object overloads numification to act like a fractional seconds when used as such.
+
+=method new($fractional)
+
+This creates a new C<Time::Spec> object from a fractional amount of time.
+
+=method new_from_pair($seconds, $nano_seconds)
+
+This creates a new C<Time::Spec> object from the second and nano second values.
+
+=method sec()
+
+This returns the number of whole seconds.
+
+=method nsec()
+
+This returns the fractional part of the time specification in nanoseconds.
+
+=method to_float()
+
+Convert the time back into fractional seconds.
+
+=method to_pair()
+
+This returns a pair of seconds an nanoseconds of the object.
